73dd5d3a46bdd7dda1fac64d4348727fdf850c6b
MICH <michstudio.com@gmail.com>
2026-01-19 10:09:46 -0600

    feat: Reorder sections for better flow
    Adjusts the order of several components in the App.tsx file. The "SecurityFilms" section is moved after "Process" and "LegalityWarranty" is moved after "SecurityFilms" to create a more logical progression for the user experience on the homepage.

--- App.tsx
+++ App.tsx
@@ -63,10 +63,10 @@ const App: React.FC = () => {
             <TikTokVideos />
             <ProductInfo onOpenModal={openQuoteModal} /> 
             <Tones onOpenModal={openQuoteModal} />
-            <SecurityFilms onOpenModal={openQuoteModal} />
             <Gallery />
-            <LegalityWarranty onOpenModal={openQuoteModal} />
             <Process onOpenModal={openQuoteModal} />
+            <SecurityFilms onOpenModal={openQuoteModal} />
+            <LegalityWarranty onOpenModal={openQuoteModal} />
             <Pricing onOpenModal={openQuoteModal} />
             <Brands onOpenModal={openQuoteModal} />
             <Business onOpenModal={openBusinessModal} />
